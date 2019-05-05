777d0a935d4866fb0af2319d56e6255f8a801a50
潘尚奇 <shangqi.pan@17zuoye.com>
2019-05-05 17:08:30 +0800

    标题栏

--- app/src/main/java/com/app/yuejuanjiazhang/MainActivity.java
+++ app/src/main/java/com/app/yuejuanjiazhang/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -28,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            //设置修改状态栏
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //设置状态栏的颜色，和你的app主题或者标题栏颜色设置一致就ok了
+            window.setStatusBarColor(getResources().getColor(R.color.theme_color));
+        }
 
         webView = (WebView) findViewById(R.id.webView);
         f5Btn = (Button)findViewById(R.id.f5_btn);
--- app/src/main/java/com/app/yuejuanjiazhang/Public.java
+++ app/src/main/java/com/app/yuejuanjiazhang/Public.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 public class Public extends Application {
-    public static String ENV = "development";
+    public static String ENV = "development1";
     public static String cookiestr = "";
     public static Context context = null;
     @Override
